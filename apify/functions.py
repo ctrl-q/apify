b88cb93334220b2ee481889460e063dc97233b4c
ctrl-q <34975747+ctrl-q@users.noreply.github.com>
2018-12-29 12:38:48 -0500

    Add get_list_of_actors()

--- apify/functions.py
+++ apify/functions.py
@@ -24,3 +24,27 @@ def get_list_of_crawlers(session=requests.session(), config="apify_config.json",
     r = session.get(url, params=kwargs)
     common.raise_for_status(r)
     return r.json()
+
+
+def get_list_of_actors(session=requests.session(), config="apify_config.json", **kwargs):
+    """Gets list of actors a user created or used
+    https://www.apify.com/docs/api/v2#/reference/actors/actor-collection/get-list-of-actors
+
+    Args:
+        session (requests.Session object): used to send the HTTP requests (default: new session)
+        config (str, path-like): path to JSON file with user ID and token
+    kwargs:
+        my (int): if 1, only actors owned by the user are returned (default: 0)
+        offset (int): rank of first request to return (default: 0)
+        limit (int): maximum number of page results to return (default: 10000)
+        desc (int): If 1, executions are sorted from newest to oldest (default: None)
+
+    Returns:
+        actor_list (JSON object): basic information about each crawler
+    """
+    user_id, token = common._get_auth(config)
+    url = "https://api.apify.com/v1/" + user_id + "/crawlers"
+    kwargs.setdefault("token", token)
+    r = session.get(url, params=kwargs)
+    common.raise_for_status(r)
+    return r.json()
