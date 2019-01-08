d669e7d489320c71f07bff7fdd53ec01bfdb0a69
ctrl-q <34975747+ctrl-q@users.noreply.github.com>
2019-01-08 14:59:33 -0500

    Update version for PyPi upload

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/ctrl-q/apify'
 EMAIL = ''
 AUTHOR = 'ctrl-q'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
